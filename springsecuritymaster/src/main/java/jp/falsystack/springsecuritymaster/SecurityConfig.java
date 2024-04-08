8c96b2b571c3f2968733e19f82e8376e872186c4
falsystack <falsystack@gmail.com>
2024-04-09 07:15:43 +0900

    Remember-me

--- springsecuritymaster/src/main/java/jp/falsystack/springsecuritymaster/SecurityConfig.java
+++ springsecuritymaster/src/main/java/jp/falsystack/springsecuritymaster/SecurityConfig.java
@@ -2,9 +2,13 @@ package jp.falsystack.springsecuritymaster;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -14,11 +18,21 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .httpBasic(basic -> basic.authenticationEntryPoint((request, response, authException) -> {
-                    response.setHeader("WWW-Authenticate", "Basic realm=security-master");
-                    response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
-                }));
+                .formLogin(Customizer.withDefaults())
+                .rememberMe(rememberMe -> rememberMe
+//                            .alwaysRemember(true)
+                            .tokenValiditySeconds(3600)
+                            .userDetailsService(userDetailsService())
+                            .rememberMeParameter("remember")
+                            .rememberMeCookieName("remember")
+                            .key("security"));
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
